d195313ba6320ec0a8faa81708bf5304abb46b14
PJS-creator <pjisung465@gmail.com>
2026-06-23 12:47:06 +0900

    Export Korea quote provider

--- portfolio/pricing/__init__.py
+++ portfolio/pricing/__init__.py
@@ -8,10 +8,18 @@ from .alpha_vantage import (
 )
 from .base import FxProvider, PriceProvider, PriceProviderError, ProviderFxRate, ProviderQuote
 from .cache import TTLFxCache, TTLQuoteCache
+from .korea import (
+    FinanceDataReaderKoreaQuoteProvider,
+    build_korea_quote_provider,
+    normalize_korea_symbol,
+    parse_finance_data_reader_price_frame,
+)
 from .service import (
     FxUpdateStatus,
     PriceUpdateStatus,
+    is_alpha_vantage_target,
     is_auto_update_target,
+    is_korea_update_target,
     refresh_holding_quotes,
     refresh_usd_krw,
     update_us_quotes,
@@ -19,6 +27,7 @@ from .service import (
 
 __all__ = [
     "AlphaVantageQuoteProvider",
+    "FinanceDataReaderKoreaQuoteProvider",
     "FxProvider",
     "FxUpdateStatus",
     "PriceProvider",
@@ -29,9 +38,14 @@ __all__ = [
     "TTLFxCache",
     "TTLQuoteCache",
     "build_alpha_vantage_provider",
+    "build_korea_quote_provider",
+    "is_alpha_vantage_target",
     "is_auto_update_target",
+    "is_korea_update_target",
+    "normalize_korea_symbol",
     "parse_alpha_vantage_currency_exchange_response",
     "parse_alpha_vantage_global_quote_response",
+    "parse_finance_data_reader_price_frame",
     "refresh_holding_quotes",
     "refresh_usd_krw",
     "update_us_quotes",
